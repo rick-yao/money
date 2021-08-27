ddf97f0e08fbbe773a369e5c795374e58bb4bc56
Rick <rick@Rick-deMacBook-Air.local>
2021-08-27 17:55:25 +0800

    basic nav route completed

---
+++ src/views/Labels.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>labels</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "labels"
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>statistics</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Statistics"
+};
+</script>
+
+<style lang="scss" scoped></style>
