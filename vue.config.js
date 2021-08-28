47aa33132669d1a1d7a76b8b3a7e7029faabe5af
Rick <rick@Rick-deMacBook-Air.local>
2021-08-28 20:40:06 +0800

    删除掉fill的svg功能，因为会导致svg无法显示

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     const dir = path.resolve(__dirname, "src/assets/icons");
     config.module
       .rule("svg-sprite")
@@ -16,13 +16,13 @@ module.exports = {
       .use("svg-sprite-loader")
       .loader("svg-sprite-loader")
       .options({ extract: false })
-      .end()
-      .use("svgo-loader")
-      .loader("svgo-loader")
-      .tap(options => ({
-        ...options,
-        plugins: [{ removeAttrs: { attrs: "fill" } }]
-      }));
+      .end();
+    // .use("svgo-loader")
+    // .loader("svgo-loader")
+    // .tap(options => ({
+    //   ...options,
+    //   plugins: [{ removeAttrs: { attrs: "fill" } }]
+    // }));
     config
       .plugin("svg-sprite")
       // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -39,5 +39,5 @@ module.exports = {
     //   .end()
     // config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     // config.module.rule('svg').exclude.add(dir)
-  }
+  },
 };
