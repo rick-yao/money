dad8c676d9c4c3c9ed83130719a5133b34136766
rick-yao <abneryaoo@gmail.com>
2021-09-08 18:24:54 +0800

    import js文件的时候应该加vue后缀，现在修复了

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,52 +1,50 @@
 <template>
   <layout>
-    <router-view/>
+    <router-view />
     <div class="tagList">
-      <router-link class="item" v-for="tag in tags" :key="tag.id"
-                   :to="`/labels/edit/${tag.name}`">
+      <router-link
+        class="item"
+        v-for="tag in tags"
+        :key="tag.id"
+        :to="`/labels/edit/${tag.name}`"
+      >
         <span>{{ tag.name }}</span>
-        <Icon name="right"/>
+        <Icon name="right" />
       </router-link>
-
     </div>
     <Button button-name="新建标签" @click="addTag"></Button>
   </layout>
 </template>
 
 <script lang="ts">
-
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import {tagListModel} from '@/models/model-tag';
-import Icon from '@/components/Icon';
-import Button from '@/components/Button.vue';
-
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { tagListModel } from "@/models/model-tag";
+import Icon from "@/components/Icon.vue";
+import Button from "@/components/Button.vue";
 
 @Component({
-  components: {Button, Icon},
+  components: { Button, Icon },
 })
 export default class labels extends Vue {
   tags = tagListModel.fetch();
 
   addTag(): void {
-    const newTag = window.prompt('请输入标签名');
-    if (newTag == '') {
-      window.alert('标签名不能为空');
-    } else if (newTag && this.tags.indexOf(newTag) >= 0
-    ) {
-      window.alert('标签已存在');
+    const newTag = window.prompt("请输入标签名");
+    if (newTag == "") {
+      window.alert("标签名不能为空");
+    } else if (newTag && this.tags.indexOf(newTag) >= 0) {
+      window.alert("标签已存在");
       return;
     } else if (newTag) {
       tagListModel.create(newTag);
       tagListModel.save();
     }
-
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
 .tagList {
   background: white;
   display: flex;
@@ -66,7 +64,6 @@ export default class labels extends Vue {
       color: #666;
     }
   }
-
 }
 
 .addTag {
