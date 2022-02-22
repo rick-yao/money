7d5959f89b051e0d1ed835f87a687f6bf6174459
rick <abneryaoo@gmail.com>
2022-02-22 12:40:20 +0800

    change build method

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@
 const path = require("path");
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/WebsiteMoney/" : "/",
+    // publicPath: process.env.NODE_ENV === "production" ? "/WebsiteMoney/" : "/",
+    publicPath:"/",
     lintOnSave: false,
     chainWebpack: (config) => {
         const dir = path.resolve(__dirname, "src/assets/icons");
